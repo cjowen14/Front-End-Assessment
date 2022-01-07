131cdfe14c278ddc27556ee111e79b161e1b0bcb
Cory Owen <cjowen14@outlook.com>
2022-01-07 11:02:17 -0700

    Completed Part 4

---
+++ profile.js
@@ -0,0 +1,26 @@
+const color = document.querySelector("#color")
+const place = document.querySelector("#place")
+const ritual = document.querySelector("#ritual")
+
+
+function myFavoriteColor(){
+    //console.log("Purple");
+    alert("My favorite color is Purple!!");
+}
+
+function myFavoritePlace(){
+    //console.log("San Diego");
+    alert("My favorite place to visit is San Diego!");
+}
+
+//I couldn't think of a favorite ritual so I changed it to a ritual I do that bothers me
+function myLeastFavoriteRitual(){
+    //console.log("I am not sure");
+    alert("My favorite ritual is after getting a kill in any FPS game, I always have to reload my gun. I've paid dearly for that many times.")
+}
+
+
+
+color.addEventListener("click", myFavoriteColor);
+place.addEventListener("click", myFavoritePlace);
+ritual.addEventListener("click", myLeastFavoriteRitual);
