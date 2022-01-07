d8dd534253f975489ef9a1d97e7b94a8cfeed925
Cory Owen <cjowen14@outlook.com>
2022-01-07 09:51:32 -0700

    Completed Part 2

--- about.js
+++ about.js
@@ -1,14 +1,22 @@
 console.log("hello world");
 
 
+let form = document.querySelector('form#contact');
+let imageHover = document.querySelector('img');
+
+
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	//console.log('form submit');
+	alert("The Form Has Been Submitted!");
+}
+
+function compliment(){
+	alert("You are a fantastic human being!!");
 }
 
 
-let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+imageHover.addEventListener('mouseover', compliment);
